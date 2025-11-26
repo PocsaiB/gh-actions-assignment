54c61b8a78a598e97a957a4a338d8c0e248eda14
Pocsai Botond <pocsai.botond@hospitaly.hu>
2025-11-26 18:57:44 +0100

    Change unit test

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -4,7 +4,7 @@ from src.calculator import Calculator
 class TestCalculatorUnit(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
-        self.assertEqual(calc.add(1, 2), 3)
+        self.assertEqual(calc.add(1, 2), 5)
 
 if __name__ == '__main__':
     unittest.main()
